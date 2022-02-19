74ea43ed2ef45ba1c6558e41fa8282885d5fa17c
mayank144 <mayankgoel756@gmail.com>
2022-02-19 19:10:33 +0000

    Create Process.env files

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 // getting-started.js
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/IndiaToday?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoURI = process.env.MONGODB_CONNECTION_STRING
 
 const connectToMongo = async() => {
     mongoose.connection.name = 'NewDatabase1'
